7dfe05cd23b574886a10bdced81532d1a654ed2a
Julia <jufarias20002@gmail.com>
2022-01-19 21:52:42 -0300

    Iniciando o projeto

---
+++ Criando um Banco Digital com java e Orientação a Objetos/src/Banco_Digital/Cliente.java
@@ -0,0 +1,22 @@
+package Banco_Digital;
+
+public class Cliente {
+	private String nome;
+	private String telefone;
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ Criando um Banco Digital com java e Orientação a Objetos/src/Banco_Digital/IConta.java
@@ -0,0 +1,11 @@
+package Banco_Digital;
+
+public interface IConta {
+    void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(double valor, IConta contaDestino);
+	
+	void imprimirExtrato();
+}

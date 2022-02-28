325b6be0b1ebae7f9681f448d99bcfd50c5facf4
Julia <jufarias20002@gmail.com>
2022-02-28 15:47:58 -0300

    creating login page

---
+++ Agenda/src/Servlets/Agenda.java
@@ -0,0 +1,29 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/teste")
+public class Agenda extends HttpServlet {
+ 
+	
+	@Override
+protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// TODO Auto-generated method stub
+	PrintWriter out = response.getWriter();
+	
+	out.print("<html>");
+	out.print("<head>");
+	out.print("<title>Primeira servlet</title>");
+	out.print("</head>");
+	out.print("<body>");
+	out.print("<h1>Olá mundo!</h1>");
+	out.print("</html>");
+  }
+}

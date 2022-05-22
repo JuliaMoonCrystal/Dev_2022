b6bb911b3b8b9018faf2e812f7a1e6967185184e
Julia Farias da Rocha <jufarias20002@gmail.com>
2022-05-22 14:18:42 -0300

    webpack

--- Reactjs/Introdução/Projecto React/src/index.js
+++ Reactjs/Introdução/Projecto React/src/index.js
@@ -1,14 +1,45 @@
-import React from "react"
+import React, { Component } from "react"
 import ReactDom from "react-dom"
 import "./styles.css"
+import Button from "./Button"
 
-const App =()=>{
-    return(
-        <div className="App">
-        Hello World
-        </div>
-    )
+function soma(a, b) {
+    alert(a + b)
 }
 
-const rootElement=document.getElementById("root")
-ReactDom.render(<App />,rootElement)
+//const App =()=>{
+//  return(
+//    <div className="App">
+//  Seja bem-vindo
+//  <Button onClick={() => soma(10,20)} name="Julia Farias" />
+//  </div>
+// )
+//}
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            clock: 1000,
+            cup: "água"
+        }
+    }
+
+    alteraraCopo = () => {
+        this.setState({
+            cup: "refri"
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.clock}</h1>
+                <button onClick={() => this.alteraraCopo()}>{this.state.cup}</button>
+            </div>
+        )
+    }
+}
+
+const rootElement = document.getElementById("root")
+ReactDom.render(<App />, rootElement)

887182f67f7e3ce292da9b8d8b9f1f02d0f038c0
Julia Farias da Rocha <jufarias20002@gmail.com>
2022-06-07 19:57:06 -0300

    att

--- Angular/Introdução/Projeto-Angular/course-manager/src/app/app.module.ts
+++ Angular/Introdução/Projeto-Angular/course-manager/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular/Introdução/Projeto-Angular/course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+     selector:'app-course-list',
+     template:'<h1>Lista de cursos</h1>'   
+})
+
+export class CourseListComponent{
+
+}

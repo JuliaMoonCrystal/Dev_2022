340fa2da3c50cef9ed114ffe144574c9f87f5715
Julia Farias da Rocha <jufarias20002@gmail.com>
2022-12-17 17:48:32 -0300

    calculadora feita com react

---
+++ Reactjs/desafio1-calculadora/src/components/Input/index.js
@@ -0,0 +1,11 @@
+import { InputContainer } from './styles';
+
+const Input = ({value}) => {
+    return (
+      <InputContainer>
+       <input disabled value={value}/>
+      </InputContainer>
+    );
+  }
+  
+  export default Input;
